789813078e4ffdf29f71b3e66b470b3173c227f0
ProsESportu <t.hajduga@poczta.fm>
2023-02-12 12:37:58 +0100

    ga

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,76 +12,82 @@ const db = admin.firestore();
 
 export const ZsemPlan = functions
     .region("europe-central2")
-    .runWith({memory:"128MB",timeoutSeconds:24,failurePolicy:true})
+    .runWith({ memory: "128MB", timeoutSeconds: 24, failurePolicy: true })
+
     .pubsub
     .schedule("0 23,7,16 * * 1-5")
     .timeZone("Europe/Warsaw")
-    .onRun(
-        (ctx) => {
-            fetch("https://zsem.edu.pl/plany/plany/o21.html")
-                .then(async (res) => {
-                    if (res.ok) {
-                        const text = await res.text();
-                        const $ = cheerio.load(text);
-                        const table = $("table.tabela")
-                        const rows = table.find("tr")
-                        const columns: fullLesson[][][] = []
-                        const times: string[] = []
-                        rows.each((i, row) => {
-                            const cells = $(row).find("td")
-                            times.push($(row).find("td.g").text())
-                            cells.each((i, cell) => {
-                                if (!columns[i]) {
-                                    columns[i] = []
-                                }
+    .onRun((_ctx) => {
+        return fetch("https://zsem.edu.pl/plany/plany/o21.html")
+            .then(async (res) => {
+                if (res.ok) {
+                    const text = await res.text();
+                    const $ = cheerio.load(text);
+                    const table = $("table.tabela")
+                    const rows = table.find("tr")
+                    const columns: fullLesson[][][] = []
+                    const times: string[] = []
+                    rows.each((_i, row) => {
+                        const cells = $(row).find("td")
+                        times.push($(row).find("td.g").text())
+                        cells.each((i, cell) => {
+                            if (!columns[i]) {
+                                columns[i] = []
+                            }
 
-                                const obj: fullLesson[] = []
-                                const less = $(cell).find("span:has(a)")
-                                if (less.length === 0) {
-                                    const lesson = $(cell).find("span.p").text()
-                                    const teacher = {
-                                        id: $(cell).find("a.n").attr("href") || "",
-                                        short: $(cell).find("a.n").text()
-                                    }
-                                    const room = {
-                                        id: $(cell).find("a.s").attr("href") || "",
-                                        short: $(cell).find("a.s").text()
-                                    }
-                                    obj[0] = {
-                                        lesson, teacher, room
-                                    }
+                            const obj: fullLesson[] = []
+                            const less = $(cell).find("span:has(a)")
+                            if (less.length === 0) {
+                                const lesson = $(cell).find("span.p").text()
+                                const teacher = {
+                                    id: $(cell).find("a.n").attr("href") || "",
+                                    short: $(cell).find("a.n").text()
                                 }
-                                less.each((i, el) => {
-                                    const lesson = $(el).find("span.p").text()
-                                    const teacher = {
-                                        id: $(el).find("a.n").attr("href") || "",
-                                        short: $(el).find("a.n").text()
-                                    }
-                                    const room = {
-                                        id: $(el).find("a.s").attr("href") || "",
-                                        short: $(el).find("a.s").text()
-                                    }
-                                    obj[i] = {
-                                        lesson, teacher, room
-                                    }
-                                })
-                                columns[i].push(obj)
+                                const room = {
+                                    id: $(cell).find("a.s").attr("href") || "",
+                                    short: $(cell).find("a.s").text()
+                                }
+                                obj[0] = {
+                                    lesson, teacher, room
+                                }
+                            }
+                            less.each((i, el) => {
+                                const lesson = $(el).find("span.p").text()
+                                const teacher = {
+                                    id: $(el).find("a.n").attr("href") || "",
+                                    short: $(el).find("a.n").text()
+                                }
+                                const room = {
+                                    id: $(el).find("a.s").attr("href") || "",
+                                    short: $(el).find("a.s").text()
+                                }
+                                obj[i] = {
+                                    lesson, teacher, room
+                                }
                             })
+                            columns[i].push(obj)
                         })
-                        times.shift()
-                        const timeTable = columns.map(e => e.map((e, i) => {
-                            return { time: times[i], lessons: e }
-                        }))
-                        timeTable.splice(0, 2)
-                        db.collection("TimeTableData").add({ timeTable: JSON.stringify(timeTable), createdAt: new Date() })
-                    } else {
-                        functions.logger.warn(res.status, res.statusText)
-                    }
-                })
-                .catch(
-                    (e) => functions.logger.error(e),
-                );
-        },
+                    })
+                    times.shift()
+                    const timeTable = columns.map(e => e.map((e, i) => {
+                        return { time: times[i], lessons: e }
+                    }))
+                    timeTable.splice(0, 2)
+                    db.collection("TimeTableData").add({ timeTable: JSON.stringify(timeTable), createdAt: new Date() })
+                    return true
+                } else {
+                    functions.logger.warn(res.status, res.statusText)
+                    return false
+                }
+            })
+            .catch(
+                (e) => {
+                    functions.logger.error(e)
+                    return false
+                }
+
+            );
+    },
     );
 
 interface fullLesson {
